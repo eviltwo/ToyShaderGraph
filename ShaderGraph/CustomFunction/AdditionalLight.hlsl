d2fa6269b16bcaa918ee984ca31ab280f0f67179
DAIKI <daiki.evilone@gmail.com>
2026-03-29 07:26:40 +0900

    point light shadows

--- ShaderGraph/CustomFunction/AdditionalLight.hlsl
+++ ShaderGraph/CustomFunction/AdditionalLight.hlsl
@@ -2,39 +2,31 @@
 #define TOYSHADER_ADDITIONALLIGHT_INCLUDED
 
 #ifndef SHADERGRAPH_PREVIEW
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/RealtimeLights.hlsl"
 #endif
 
 #pragma multi_compile _ _ADDITIONAL_LIGHTS
 #pragma multi_compile _ _CLUSTER_LIGHT_LOOP
+#pragma multi_compile _ _ADDITIONAL_LIGHT_SHADOWS
 
 #ifndef SHADERGRAPH_PREVIEW
 float3 CalculateLighting(Light light, float3 normalWS)
 {
     float NdotL = dot(normalWS, normalize(light.direction));
     NdotL = (NdotL + 1) * 0.5;
-    return saturate(NdotL) * light.color * light.distanceAttenuation * light.shadowAttenuation;
+    return saturate(NdotL) * light.color * light.distanceAttenuation * (light.shadowAttenuation * light.shadowAttenuation);
 }
 #endif
 
-void GetAdditionalLightColor_float(float3 positionWS, float2 screenPosition, float3 normalWS, float step, out float3 color)
+void GetAdditionalLightColor_float(float3 positionWS, float2 screenPosition, float3 normalWS, float stepsRange, float steps, out float3 color)
 {
     #ifdef SHADERGRAPH_PREVIEW
     color = float3(1, 1, 1);
     #else
 
     color = float3(0, 0, 0);
-    #if defined(_ADDITIONAL_LIGHTS)
-    /*
-    // Additional light loop for non-main directional lights. This block is specific to Forward+.
-    #if USE_CLUSTER_LIGHT_LOOP
-    UNITY_LOOP for (uint lightIndex = 0; lightIndex < min(URP_FP_DIRECTIONAL_LIGHTS_COUNT, MAX_VISIBLE_LIGHTS); lightIndex++)
-    {
-        Light light = GetAdditionalLight(lightIndex, positionWS, half4(1, 1, 1, 1));
-        color += CalculateLighting(light, normalWS);
-    }
-    #endif
-    */
+    #ifdef _ADDITIONAL_LIGHTS
 
     // Make InputData for LIGHT_LOOP (Forward+)
     #if USE_CLUSTER_LIGHT_LOOP
@@ -47,12 +39,13 @@ void GetAdditionalLightColor_float(float3 positionWS, float2 screenPosition, flo
     LIGHT_LOOP_BEGIN(pixelLightCount)
     {
         Light light = GetAdditionalLight(lightIndex, positionWS);
+        light.shadowAttenuation = AdditionalLightRealtimeShadow(lightIndex, positionWS, light.direction);
         color += CalculateLighting(light, normalWS);
     }
     LIGHT_LOOP_END
     #endif
 
-    color = round(color / step) * step;
+    color = saturate(ceil(color / (stepsRange / steps)) * (1.0 / steps));
 
     #endif
 }
